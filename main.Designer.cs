8627a2f8c8d142dffd93248a18289e9763e903bb
Anıl Emir Döner <53477261+aemirdnr@users.noreply.github.com>
2019-12-19 15:48:31 +0300

    V1.0

---
+++ main.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BanApp
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtApp = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblApp = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblStat = new System.Windows.Forms.Label();
+            this.onOff = new System.Windows.Forms.Panel();
+            this.banTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(102, 185);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 27);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "BAN";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtApp
+            // 
+            this.txtApp.Location = new System.Drawing.Point(82, 58);
+            this.txtApp.Name = "txtApp";
+            this.txtApp.Size = new System.Drawing.Size(140, 22);
+            this.txtApp.TabIndex = 1;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(102, 134);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 22);
+            this.txtMin.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "App name";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtApp);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtMin);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 228);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Minute";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblApp);
+            this.groupBox2.Controls.Add(this.lblTime);
+            this.groupBox2.Controls.Add(this.lblStat);
+            this.groupBox2.Controls.Add(this.onOff);
+            this.groupBox2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 252);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(309, 228);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // lblApp
+            // 
+            this.lblApp.AutoSize = true;
+            this.lblApp.Location = new System.Drawing.Point(17, 68);
+            this.lblApp.Name = "lblApp";
+            this.lblApp.Size = new System.Drawing.Size(75, 14);
+            this.lblApp.TabIndex = 3;
+            this.lblApp.Text = "App name:";
+            this.lblApp.Visible = false;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(17, 106);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(171, 14);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "Will be closed in 0 minute.";
+            this.lblTime.Visible = false;
+            // 
+            // lblStat
+            // 
+            this.lblStat.AutoSize = true;
+            this.lblStat.Location = new System.Drawing.Point(17, 32);
+            this.lblStat.Name = "lblStat";
+            this.lblStat.Size = new System.Drawing.Size(81, 14);
+            this.lblStat.TabIndex = 1;
+            this.lblStat.Text = "Status: OFF";
+            // 
+            // onOff
+            // 
+            this.onOff.BackColor = System.Drawing.Color.Red;
+            this.onOff.Location = new System.Drawing.Point(278, 12);
+            this.onOff.Name = "onOff";
+            this.onOff.Size = new System.Drawing.Size(25, 25);
+            this.onOff.TabIndex = 0;
+            // 
+            // banTimer
+            // 
+            this.banTimer.Tick += new System.EventHandler(this.banTimer_Tick);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 492);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "main";
+            this.Text = "main";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtApp;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblApp;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblStat;
+        private System.Windows.Forms.Panel onOff;
+        private System.Windows.Forms.Timer banTimer;
+    }
+}
---
+++ main.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BanApp
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        string appname;
+        int time;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        void start()
+        {
+            if (banTimer.Enabled == false)
+            {
+                appname = txtApp.Text;
+                time = Convert.ToInt32(txtMin.Text);
+
+                if (!string.IsNullOrEmpty(appname) && !string.IsNullOrEmpty(Convert.ToString(time)))
+                {
+                    int min = time;
+                    int hr = min / 60;
+                    int sec = time * 60;
+                    int msec = sec * 1000;
+
+                    if (time >= 60)
+                    {
+                        min -= hr * 60;
+
+                        banTimer.Interval = msec;
+
+                    }
+                    else
+                    {
+                        banTimer.Interval = msec;
+                    }
+
+                    //Results gbox
+                    lblStat.Text = "Status: ON";
+                    lblApp.Text = "App name: " + appname;
+                    lblApp.Show();
+                    lblTime.Text = "Will be closed in " + time + " minute.";
+                    lblTime.Show();
+                    onOff.BackColor = Color.Green;
+
+                    banTimer.Start();
+                }
+                else
+                {
+                    MessageBox.Show("App name or minute is empty!");
+                }
+            }
+            else
+            {
+                DialogResult ctrl = new DialogResult();
+                ctrl = MessageBox.Show("Cancel Process", "There is have already process, Do you want cancel process?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (ctrl == DialogResult.Yes)
+                {
+                    //cancel process
+                    banTimer.Stop();
+
+                    //Results gbox
+                    lblStat.Text = "Status: OFF";
+                    lblApp.Hide();
+                    lblTime.Hide();
+                    onOff.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void banTimer_Tick(object sender, EventArgs e)
+        {
+            appname = txtApp.Text;
+            Process[] processes = Process.GetProcesses();
+
+            //find appname in process and kill.
+            foreach (Process appList in processes)
+            {
+                if (appList.ProcessName == appname)
+                {
+                    appList.Kill();
+                    
+                    //Results gbox
+                    lblStat.Text = "Status: OFF";
+                    lblApp.Hide();
+                    lblTime.Hide();
+                    onOff.BackColor = Color.Red;
+
+                    banTimer.Stop();
+                }
+            }
+        }
+    }
+}
