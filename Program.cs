626e7aae9c6b4a1e0a2615ef6ca15135fa4d68b9
Anıl Emir Döner <53477261+aemirdnr@users.noreply.github.com>
2019-12-20 12:05:57 +0300

    V1.1

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace BanApp
 {
     static class Program
     {
-        /// <summary>
-        /// Uygulamanın ana girdi noktası.
-        /// </summary>
         [STAThread]
         static void Main()
         {
